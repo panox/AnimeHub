d6f7f0724e23dfe712fe5fc330d66db8847871d8
panox <panospalos@gmail.com>
2015-12-12 14:59:57 +0000

    removed jwt

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 angular
-  .module("animeHub", ['ngResource', 'angular-jwt'])
+  .module("animeHub", ['ngResource'])
   .constant('API', window.location.hostname.match('localhost') ? 'localhost:3000/api/' : 'animehub.herokuapp.com/api/');
