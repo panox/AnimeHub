efcef85aba3220fc9f41f6125fe44ae229d8ae1d
panox <panospalos@gmail.com>
2015-12-12 14:35:02 +0000

    anime controller

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
 angular
   .module("animeHub", ['ngResource', 'angular-jwt'])
   .constant('API', $window.location.hostname.match('localhost') ? 'localhost:3000/api/' : 'animehub.herokuapp.com/api/')
+
 angular
   .module("animeHub")
   .factory('Anime', Anime);
