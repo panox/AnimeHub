dce62e1f8ef13c77f580e32c4ba30c6ae3493cdd
panox <panospalos@gmail.com>
2015-12-12 16:01:03 +0000

    users model

---
+++ src/js/models/user.js
@@ -0,0 +1,13 @@
+angular
+  .module("animeHub")
+  .factory('User', User);
+
+User.$inject = ['$resource', 'API'];
+function User($resource, API) {
+
+  return $resource(API + 'users/:id', null, {
+    'login':{method: "POST", url: API + 'login'},
+    'signup':{method: "POST", url: API + 'signup'}
+  });
+  
+}
