a68f91c6cc8c85d117fdc760bbc2b9d94b050a9b
panox <panospalos@gmail.com>
2015-12-12 18:31:23 +0000

    routes for anime login signup works

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,30 @@
 angular
-  .module("animeHub", ['ngResource', 'angular-jwt'])
-  .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'http://animehub.herokuapp.com/api/');
+  .module("animeHub", ['ngResource', 'angular-jwt', 'ui.router'])
+  .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'http://animehub.herokuapp.com/api/')
+  .config(MainRouter);
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', { // 'home' is a name so we can refer to this state
+      url: '/', // a relative url so angular can match the route to this state
+      templateUrl: "partials/home.html" // the name of the file that contains our html for this stat ( a template )
+    });
+
+  $stateProvider
+    .state('login', { 
+      url: '/login',
+      templateUrl: "partials/login.html"
+    });
+
+  $stateProvider
+    .state('signup', { 
+      url: '/signup',
+      templateUrl: "partials/signup.html"
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
 angular
   .module("animeHub")
   .controller("animesController", animesController);
