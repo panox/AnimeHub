34a44d609afb72026a6f18629803b73165576f78
panox <panospalos@gmail.com>
2015-12-12 15:13:09 +0000

    gulp build

--- js/javascript.js
+++ js/javascript.js
@@ -3,18 +3,9 @@ angular
   .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api' : 'animehub.herokuapp.com/api/');
 angular
   .module("animeHub")
-  .factory('Anime', Anime);
-
-Anime.$inject = ['$resource'];
-
-function Anime($resource) {
-}
-angular
-  .module("animeHub")
   .controller("animeController", animeController);
 
-animeController.$inject = ['Anime'];
-function animeController(Anime){
+function animeController(){
 
   var self = this;
 
