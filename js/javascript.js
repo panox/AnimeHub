c0188fd42d52a79fe7ef097730808689f86ecc0c
panox <panospalos@gmail.com>
2015-12-12 14:14:24 +0000

    anime factory

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,14 @@
 angular
   .module("animeHub", ['ngResource', 'angular-jwt'])
-  .constant('API', $window.location.hostname.match('localhost') ? 'localhost:3000/api' : 'animehub.herokuapp.com/api')
+  .constant('API', $window.location.hostname.match('localhost') ? 'localhost:3000/api/' : 'animehub.herokuapp.com/api/')
+angular
+  .module("animeHub")
+  .factory('Anime', Anime);
+
+Anime.$inject = ['$resource'];
+
+function Anime($resource) {
+  // Rsource class
+  return $resource(API + 'anime/:id', null, {
+  });
+};
