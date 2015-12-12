53b270505d221442d8ceef168e8ec77974168dcd
panox <panospalos@gmail.com>
2015-12-12 16:06:01 +0000

    user controller initial method

---
+++ src/js/controllers/usersController.js
@@ -0,0 +1,11 @@
+angular
+  .module("animeHub")
+  .controller("usersController", usersController);
+
+usersController.$inject = ['User'];
+function usersController(User){
+
+  // object saved as self
+  var self = this;
+
+}
