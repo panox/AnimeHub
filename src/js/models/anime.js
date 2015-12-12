6655a2377a35c3efd685940de679a8688aa597e4
panox <panospalos@gmail.com>
2015-12-12 15:57:37 +0000

    indent

--- src/js/models/anime.js
+++ src/js/models/anime.js
@@ -6,6 +6,6 @@ Anime.$inject = ['$resource', 'API']
 
 function Anime($resource, API) {
   return $resource(API + 'anime/:id', null, {
-  'query': { method:'get', url: API + 'animes' }
+    'query': { method:'get', url: API + 'animes' }
   })
 }
