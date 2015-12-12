6ffc584359b15832f94fe0f30defb8b851e44bfd
panox <panospalos@gmail.com>
2015-12-12 16:22:31 +0000

    token service

---
+++ src/js/services/tokenService.js
@@ -0,0 +1,17 @@
+angular
+  .module("animeHub")
+  .service('TokenService', TokenService);
+
+TokenService.$inject = ['$window', 'jwtHelper'];
+function TokenService($window, jwtHelper) {
+  var self = this;
+
+  self.saveUserToken = function(token) {
+    $window.localStorage.setItem('userToken', token)
+  };
+
+  self.getUserToken = function() {
+    return $window.localStorage.getItem('userToken')
+  };
+
+}
