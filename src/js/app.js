a8591a2ba8d6fd8a05cd67134d66fb3265460d9a
panox <panospalos@gmail.com>
2015-12-12 18:25:03 +0000

    home router works

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,8 @@ function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', { // 'home' is a name so we can refer to this state
       url: '/', // a relative url so angular can match the route to this state
-      templateUrl: "home.html" // the name of the file that contains our html for this stat ( a template )
-    })
+      templateUrl: "partials/home.html" // the name of the file that contains our html for this stat ( a template )
+    });
 
     $urlRouterProvider.otherwise('/');
-};
+}
