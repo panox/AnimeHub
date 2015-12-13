d4f64169d8d34cc8f2fea89f261edf53c4049bb2
panox <panospalos@gmail.com>
2015-12-13 15:23:18 +0000

    create method consoles create

--- src/js/controllers/commentsController.js
+++ src/js/controllers/commentsController.js
@@ -6,6 +6,10 @@ commentsController.$inject = ['Comment'];
 function commentsController(Comment){
   var self = this;
 
+  self.create = function() {
+    console.log('create')
+  };
+
   self.removeComment = function(id) {
     console.log('delete');
   };
