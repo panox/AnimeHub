76502d97658f47fd55e39ee3df1732da4fbc4256
panox <panospalos@gmail.com>
2015-12-13 16:41:30 +0000

    removed comments conroller file

--- js/javascript.js
+++ js/javascript.js
@@ -84,36 +84,6 @@ function animesController($stateParams, Anime, Comment, TokenService){
 }
 angular
   .module("animeHub")
-  .controller("commentsController", commentsController);
-
-commentsController.$inject = ['Comment', 'TokenService'];
-function commentsController(Comment, TokenService){
-  var self = this;
-
-  self.comment = {};
-
-  self.userToken = TokenService.getUser();
-
-  self.create = function(animeId) {
-    self.comment.user = self.userToken._id;
-    console.log(self.comment);
-    Comment.save(
-      { animeId: animeId },
-      self.comment, 
-      function(res) {
-        console.log(res);
-      }, function(err) {
-        console.log(err.data.message);
-      }
-    );
-  };
-
-  self.removeComment = function(id) {
-    Comment.remove({id:id});
-  };
-}
-angular
-  .module("animeHub")
   .controller("usersController", usersController);
 
 usersController.$inject = ['User', 'TokenService'];
