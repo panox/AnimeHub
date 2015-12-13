6978458c526bbbaa7de4e0a524cd1e4684980467
panox <panospalos@gmail.com>
2015-12-13 15:44:19 +0000

    comment save sucess and fail

--- src/js/controllers/commentsController.js
+++ src/js/controllers/commentsController.js
@@ -7,7 +7,15 @@ function commentsController(Comment){
   var self = this;
 
   self.create = function() {
-    console.log('create')
+    console.log('create');
+    console.log(self.comment);
+    Comment.save({ animeId: 21034 }, self.comment, 
+      function(res) {
+        console.log(res)
+      }, function(err) {
+        console.log(err)
+      };
+    )
   };
 
   self.removeComment = function(id) {
