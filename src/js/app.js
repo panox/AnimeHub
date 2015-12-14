08d1c51c5c9ef0781e1cce15c1f83ffeef580b82
panox <panospalos@gmail.com>
2015-12-14 10:21:47 +0000

    add interceptro

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 angular
   .module("animeHub", ['ngResource', 'angular-jwt', 'ui.router'])
   .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'http://animehub.herokuapp.com/api/')
-  .config(MainRouter);
+  .config(MainRouter)
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  });
 
 MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 function MainRouter($stateProvider, $urlRouterProvider) {
