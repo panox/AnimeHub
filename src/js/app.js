db2b9cb8f55170f4460e601e3c661ab7e43e115a
panox <panospalos@gmail.com>
2015-12-15 14:28:59 +0000

    root constant

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ angular
   .module("animeHub", ['ngResource', 'angular-jwt', 'ui.router'])
   .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'https://animehub-api.herokuapp.com/api/')
   .constant('CLIENT', 'http://5734940f.ngrok.com/#/')
+  .constant('ROOT', window.location.hostname.match('localhost') ? '/' : '/AnimeHub/#/')
   .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
   });
