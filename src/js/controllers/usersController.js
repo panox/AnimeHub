49226f068ec966c7e7dcf32a62c834b270789ab9
panox <panospalos@gmail.com>
2015-12-15 18:54:19 +0000

    function for response of token creation

--- src/js/controllers/usersController.js
+++ src/js/controllers/usersController.js
@@ -19,7 +19,28 @@ function usersController(User, TokenService, $window, ROOT){
 
   // payment method
   self.pay = function() {
-    console.log('pay')
+    var $form = $('#payment-form');
+    // disable button after first click
+    $form.find('button').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
+  };
+
+  // function with reponse from stripe create token
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $form.get(0).submit();
+    }
   };
 
   // method to login
