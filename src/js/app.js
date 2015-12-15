9c97235729448a1da3c92e15277bde68ebf45157
panox <panospalos@gmail.com>
2015-12-15 09:19:20 +0000

    changed api heroku constant

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 angular
   .module("animeHub", ['ngResource', 'angular-jwt', 'ui.router'])
-  .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'http://animehub.herokuapp.com/api/')
+  .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'https://animehub-api.herokuapp.com/api/')
   .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
   });
