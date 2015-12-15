cbfdd5846fd3260650684fc69180c52e289e9495
panox <panospalos@gmail.com>
2015-12-15 21:01:27 +0000

    client constant for dev and production

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 angular
   .module("animeHub", ['ngResource', 'angular-jwt', 'ui.router'])
   .constant('API', window.location.hostname.match('localhost') ? 'http://localhost:3000/api/' : 'https://animehub-api.herokuapp.com/api/')
-  .constant('CLIENT', 'http://5734940f.ngrok.com/#/')
+  .constant('CLIENT', window.location.hostname.match('panox.github.io') ? 'http://panox.github.io/AnimeHub/#/' : 'http://5734940f.ngrok.com/#/')
   .constant('ROOT', window.location.hostname.match('localhost') ? '/' : '/AnimeHub/#/')
   .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
