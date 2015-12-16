00978965628f214ef9c68546bc908a5d6246e738
panox <panospalos@gmail.com>
2015-12-16 11:06:25 +0000

    facebook controller

---
+++ src/js/controllers/facebookController.js
@@ -0,0 +1,8 @@
+angular
+  .module("animeHub")
+  .controller("facebookController", facebookController);
+
+  facebookController.$inject = ['$window'];
+function facebookController($window){
+  var self = this;
+}
