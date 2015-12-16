8a7b215ab34755374a9e847d407e96582f225d21
panox <panospalos@gmail.com>
2015-12-16 15:26:12 +0000

    nav initial style

--- js/javascript.js
+++ js/javascript.js
@@ -42,99 +42,6 @@ angular
   }
 angular
   .module("animeHub")
-  .factory('Anime', Anime);
-
-Anime.$inject = ['$resource', 'API'];
-
-function Anime($resource, API) {
-  return $resource(API + 'anime/:id', null, {
-    'query': { method:'get', url: API + 'animes' }
-  });
-}
-angular
-  .module("animeHub")
-  .factory('Comment', Comment);
-
-Comment.$inject = ['$resource', 'API'];
-function Comment($resource, API) {
-
-  return $resource(API + 'comment/:id', null, {
-    'update': { method:'PUT', url: API + 'comment/:id' },
-    'save': { method:'POST', url: API + 'anime/:animeId/comments'}
-  });
-  
-}
-angular
-  .module("animeHub")
-  .factory('User', User);
-
-User.$inject = ['$resource', 'API'];
-function User($resource, API) {
-
-  return $resource(API + 'users/:id', null, {
-    'login':{method: "POST", url: API + 'login'},
-    'signup':{method: "POST", url: API + 'signup'},
-    'update': { method:'PUT'},
-    'pay': {method: 'POST', url: API + 'users/pay'}
-  });
-  
-}
-angular
-  .module("animeHub")
-  .factory('AuthInterceptor', AuthInterceptor);
-
-function AuthInterceptor(API, TokenService) {
-  return {
-
-    request: function(config) {
-      var token = TokenService.getUserToken();
-
-      if(config.url.match(API) && token) {
-        config.headers.Authorization = 'Bearer ' + token;
-      }
-
-      return config;
-    },
-
-    response: function(res) {
-      if(res.config.url.match(API) && res.data.token) {
-        TokenService.saveUserToken(res.data.token);
-      }
-
-      return res;
-    }
-  
-  };
-  
-}
-angular
-  .module("animeHub")
-  .service('TokenService', TokenService);
-
-TokenService.$inject = ['$window', 'jwtHelper'];
-function TokenService($window, jwtHelper) {
-  var self = this;
-
-  self.saveUserToken = function(token) {
-    $window.localStorage.setItem('userToken', token);
-  };
-
-  self.getUserToken = function() {
-    return $window.localStorage.getItem('userToken');
-  };
-
-  self.removeUserToken = function() {
-    $window.localStorage.removeItem('userToken');
-  };
-
-  self.getUser = function() {
-    var token = self.getUserToken();
-    return jwtHelper.decodeToken(token);
-  };
-
-}
-angular
-  .module("animeHub")
   .controller("animesController", animesController);
 
 animesController.$inject =['$stateParams', 'Anime', 'Comment', 'TokenService', 'CLIENT'];
@@ -422,6 +329,99 @@ function usersController(User, TokenService, $window, ROOT, $state){
         }
       );
     });
+  };
+
+}
+angular
+  .module("animeHub")
+  .factory('Anime', Anime);
+
+Anime.$inject = ['$resource', 'API'];
+
+function Anime($resource, API) {
+  return $resource(API + 'anime/:id', null, {
+    'query': { method:'get', url: API + 'animes' }
+  });
+}
+angular
+  .module("animeHub")
+  .factory('Comment', Comment);
+
+Comment.$inject = ['$resource', 'API'];
+function Comment($resource, API) {
+
+  return $resource(API + 'comment/:id', null, {
+    'update': { method:'PUT', url: API + 'comment/:id' },
+    'save': { method:'POST', url: API + 'anime/:animeId/comments'}
+  });
+  
+}
+angular
+  .module("animeHub")
+  .factory('User', User);
+
+User.$inject = ['$resource', 'API'];
+function User($resource, API) {
+
+  return $resource(API + 'users/:id', null, {
+    'login':{method: "POST", url: API + 'login'},
+    'signup':{method: "POST", url: API + 'signup'},
+    'update': { method:'PUT'},
+    'pay': {method: 'POST', url: API + 'users/pay'}
+  });
+  
+}
+angular
+  .module("animeHub")
+  .factory('AuthInterceptor', AuthInterceptor);
+
+function AuthInterceptor(API, TokenService) {
+  return {
+
+    request: function(config) {
+      var token = TokenService.getUserToken();
+
+      if(config.url.match(API) && token) {
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+
+      return config;
+    },
+
+    response: function(res) {
+      if(res.config.url.match(API) && res.data.token) {
+        TokenService.saveUserToken(res.data.token);
+      }
+
+      return res;
+    }
+  
+  };
+  
+}
+angular
+  .module("animeHub")
+  .service('TokenService', TokenService);
+
+TokenService.$inject = ['$window', 'jwtHelper'];
+function TokenService($window, jwtHelper) {
+  var self = this;
+
+  self.saveUserToken = function(token) {
+    $window.localStorage.setItem('userToken', token);
+  };
+
+  self.getUserToken = function() {
+    return $window.localStorage.getItem('userToken');
+  };
+
+  self.removeUserToken = function() {
+    $window.localStorage.removeItem('userToken');
+  };
+
+  self.getUser = function() {
+    var token = self.getUserToken();
+    return jwtHelper.decodeToken(token);
   };
 
 }
