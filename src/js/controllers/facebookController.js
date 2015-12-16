5ab707d2cf8a7b199b791e3ee20332948c17d48f
panox <panospalos@gmail.com>
2015-12-16 11:07:27 +0000

    facebook init method

--- src/js/controllers/facebookController.js
+++ src/js/controllers/facebookController.js
@@ -5,4 +5,24 @@ angular
   facebookController.$inject = ['$window'];
 function facebookController($window){
   var self = this;
+
+  self.init = function() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1664979030450339',
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  };
+
+  self.init;
 }
